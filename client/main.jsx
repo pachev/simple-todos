0bbb6a91b1c9ee60da7272ce985f4ab091920747
Pachev Joseph <pachevjoseph@gmail.com>
2016-05-29 17:35:33 -0400

    Added todo list item creation and mongodb storage

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../imports/api/tasks.js'
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
- 
+
 import App from '../imports/ui/App.jsx';
- 
+
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
 });
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
- 
+
 // Task component - represents a single todo item
 export default class Task extends Component {
   render() {
@@ -8,7 +8,7 @@ export default class Task extends Component {
     );
   }
 }
- 
+
 Task.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required
